83e8fc4cab40a61244cf6d71093a1894a3473d10
Luis Zabala <luis.zabala076@gmail.com>
2019-01-29 18:19:02 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,19 @@
 class Author
   attr_accessor :name
+  
+  @@post_count = []
+  
+  def initialize(name)
+    @name = name
+    @@post_count << self
+  end
+  
+  def posts
+    @@post_count << self
+  end
+  
+  def add_post(name)
+    name = Post.new(name)
+    name.title = self.
+end
 end
